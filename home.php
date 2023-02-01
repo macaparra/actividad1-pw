ffddd2f06bdf59ed4566220d22c47a96628456a5
Macarena <parrameca@gmail.com>
2023-02-01 15:09:16 -0400

    First commit

---
+++ home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css"rel="stylesheet"/>
+    <title>Actividad 1</title>
+</head>
+<body>
+    <h1>Ingrese los datos a los siguientes 5 Empleados: &#128590</h1>
+    <form action="index.php" method="post" >
+        <?php
+            $amount = 5;
+            for($i=0;$i<$amount;$i++){
+            echo"&#128590 Empleado ".$i+1;
+        ?>
+            <div>
+                <br>
+                <label class="title" for="nombreid"> &#128204 Nombre y Apellido:</label> <br>
+                <input type="text" name="nombre<?php echo $i?>" id="nombreid" require="" pattern="[a-zA-Z]+" oninvalid="this.setCustomValidity('Solo puedes ingresar letas')"
+                oninput="this.setCustomValidity('')" ><br><br>
+            
+                <label class="title" for="edadid">&#128204 Edad:</label> <br>
+                <input type="number" name="edad<?php echo $i?>" id="edadid"><br><br><br>
+        
+                <label class="title" for="estadoid">&#128204 Estado Civil:</label> <br>
+                <input type="radio" id="solteroid" name="estado<?php echo $i?>" value="soltero">
+                <label for="soltero">Soltero(a)</label>
+    
+                <input type="radio" id="casadoid" name="estado<?php echo $i?>" value="casado">
+                <label for="casado">Casado(a)</label>
+             
+                <input type="radio" id="viudoid" name="estado<?php echo $i?>" value="viudo">
+                <label for="viudo">Viudo(a)</label>
+
+                <br><br>
+                
+                <label class="title" for="generoid">&#128204 Genero:</label> <br>
+                <input type="radio" id="femeninoid" name="genero<?php echo $i?>" value="femenino">
+                <label for="femenino">Femenino</label>
+        
+                <input type="radio" id="masculinoid" name="genero<?php echo $i?>" value="masculino">
+                <label for="masculino">Masculino</label>
+              
+                <br><br>
+            
+                <label class="title" for="sueldo">&#128204 Sueldo:</label> <br>
+                <input type="radio" id="menosid" name="sueldo<?php echo $i?>" value="menos">
+                <label for="menos">Menos de 1000 Bs</label>
+            
+                <input type="radio" id="entreid" name="sueldo<?php echo $i?>" value="entre">
+                <label for="entre">Entre 1000 y 2500 Bs</label>
+            
+                <input type="radio" id="masid" name="sueldo<?php echo $i?>" value="mas">
+                <label for="mas">Mas de 2500 Bs</label>
+            
+            </div>
+            <br><br>
+            <hr class="line"><br>
+        <?php
+            }
+        ?>
+        <button class="boton" name="btn" value="Enviar" type="submit"> Enviar</button>
+        <input class="boton" type="reset" name="btn" value="Limpiar">
+        <br><br>
+</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+    $femenino = 0;
+    $casado = 0;
+    $viudo = 0;
+    $masculino = 0;
+    $edades = 0;
+    $data = $_POST;
+    $flag = false;    
+    $amount = 5;
+        for($i=0;$i<$amount;$i++){
+            if( !empty($data['genero'.$i])&&!empty($data['estado'.$i])&&!empty($data['sueldo'.$i])&&!empty($data['nombre'.$i])&&!empty($data['edad'.$i]) ) {
+                if($data['genero'.$i]==='femenino')
+                $femenino++;
+                if($data['estado'.$i]==='casado' && $data['sueldo'.$i]==='mas'&& $data['genero'.$i]==='masculino' )
+                $casado++;
+                if($data['estado'.$i]==='viudo' && $data['sueldo'.$i]==='entre' && $data['genero'.$i]==='femenino')
+                $viudo++;
+                if($data['genero'.$i]==='masculino'){
+                    $edades+=$data['edad'.$i];
+                    $masculino++;
+                }
+            }else{
+                $flag = true;
+            }
+        }
+        $promedio = $masculino>0&&$edades>0?$edades/$masculino:0;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css"rel="stylesheet"/>
+    <title>Actividad 1</title>
+</head>
+<body>
+<div>
+    <?php
+        if($flag){
+    ?>
+        <div class="mensaje">
+            <h1>Debe llenar todos los datos&#10071&#10071</h1>
+            <button class="boton" type="submit" onClick="document.location.href='home.php'"> Volver</button>
+        </div>
+    <?php }else{ ?>
+    <h1>&#128203 Resultados:</h1>
+    <div class="resultados">
+        <label for="empleadosFemeninoId">&#128204 Total de empleados del genero femenino: <?php echo $femenino?></label> <br><br>
+        <label for="empleadosCasadoId">&#128204 Total de hombres casados que ganan mas de 2500 Bs: <?php echo $casado?></label> <br><br>
+        <label for="empleadosViudaId"> &#128204 Total de mujeres viudas que ganan mas de 1000 Bs: <?php echo $viudo?></label> <br><br>
+        <label for="edadPromedioHombre"> &#128204 Edad promedio de los hombres: <?php echo round($promedio)?></label> <br><br>
+        <button class="boton" type="submit" onClick="document.location.href='home.php'"> Volver</button>
+    </div>
+    <?php
+        }
+    ?>
+</div>
+</body>
+</html>
